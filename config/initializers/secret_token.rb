77e577bf2c5eeee6d04d2220218f9f34d9378925
pz <zabilamailbox@mail.ru>
2013-11-02 23:22:38 +0400

    one

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewHer::Application.config.secret_key_base = 'ce7db97e0a10c4e72189ef76ef372bce683c6c0795ba56d656a72187aa6f43d928d7591b8ea3fd69af02001e66dc45160ea41587e8ac51822a037ac3654f5962'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NewHer::Application.config.session_store :cookie_store, key: '_new_her_session'
